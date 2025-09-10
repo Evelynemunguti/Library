56282122b5e55562aa146751b4649a77a8cb3e42
Evelyne Munguti <evelynekyamwai@gmail.com>
2025-09-10 16:45:22 +0300

    added the function to state the book name,author , pages and whether the book has been read or not yet

---
+++ library.js
@@ -0,0 +1,13 @@
+function Book(title,author,pages,hasRead){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasread = hasRead;
+}
+
+Book.prototype.info= function(){
+    return `${this.title} by ${this.author} ${this.pages} pages` + (this.hasRead? "You have read ":"not read yet");
+};
+
+const a = new Book("The Hobbit","Tolkein",256, false);
+console.log(a.info());
